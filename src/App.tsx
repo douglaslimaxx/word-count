20ecc311c264a117d0174ad3431e4dfccbb3ff53
Douglas Lima <douglas.lima@ccc.ufcg.edu.br>
2022-08-15 18:19:45 -0300

    challenge complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [value, setValue] = useState("")
+
+  const [wCount, setWCount] = useState(0)
+
+  const [classNameInput, setClassNameInput] = useState({
+    input: "wordInput", 
+    errorMessage: "hideMessageError",
+    resultMessage: "hideMessageResult"
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="Word Count">
+        Word Count
       </header>
+       <div>
+        <input 
+        className={classNameInput.input}
+        type="text" 
+        placeholder='Put the text here'
+        value={value} 
+        onChange={e => {
+          setClassNameInput({
+            input: "wordInput", 
+            errorMessage: "hideMessageError",
+            resultMessage: "hideMessageResult"})
+          setValue(e.target.value)}}/>
+       </div>
+      <button onClick={() => {
+          const number = value.split(" ").filter((i) => i !== "").length
+          if(number === 0){
+            setClassNameInput({
+              input: "wordInputError", 
+              errorMessage: "showMessageError", 
+              resultMessage: "hideMessageResult"})
+            setWCount(number)
+          } else {
+            setClassNameInput({
+              input: "wordInput", 
+              errorMessage: "hideMessageError", 
+              resultMessage: "showMessageResult"})
+            setWCount(number)
+          }
+          }
+        }>Submit</button>
+      <div
+      className={classNameInput.resultMessage}>
+        This text contains {wCount} words
+      </div>
+      <div
+      className={classNameInput.errorMessage}>
+        No text detected - Text is required
+      </div>
     </div>
   );
 }
