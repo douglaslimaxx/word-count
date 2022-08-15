257044f5cb379ea9cf2e102c93ba6a9b69d7333e
Douglas Lima <douglas.lima@ccc.ufcg.edu.br>
2022-08-15 18:33:47 -0300

    Updates

---
+++ precache-manifest.2dfc436af1f1dcacdf4c51cf3caf3733.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b39c59e664d8eda96284",
+    "url": "/word-count/static/css/main.88d9fb3f.chunk.css"
+  },
+  {
+    "revision": "b39c59e664d8eda96284",
+    "url": "/word-count/static/js/main.b39c59e6.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/word-count/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/word-count/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "c598de1c9f742fced11a",
+    "url": "/word-count/static/js/runtime~main.c598de1c.js"
+  },
+  {
+    "revision": "173e8684cb8fe5689766600d70ce9b51",
+    "url": "/word-count/index.html"
+  }
+];
